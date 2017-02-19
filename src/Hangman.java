9d0f85b66ec2d34793376db668e22642983d07d0
Baris <b.imre@student.utwente.nl>
2017-02-19 22:52:32 +0100

    started

---
+++ src/Hangman.java
@@ -0,0 +1,195 @@
+/**
+ * _This class creates a Hangman object for the game __
+ *
+ * @author _Berfin KÃœÃ‡ÃœK__
+ * @author _Ã‡aÄŸla SÃ¶zen__
+ * @author _Ä°dil HANHAN__
+ * @author _Merve SAÄ�YATANLAR__
+ * @author _YaÄŸÄ±z Efe MERTOL__
+ * @version _11.02.2017_
+ */
+public class Hangman {
+    // properties
+
+    //constants
+
+    private int MAX_ALLOWED_INCORRECT_TRIES;
+
+
+    //variables
+
+    private StringBuffer secretWord;
+    private StringBuffer allLetters = new StringBuffer();;
+    private StringBuffer usedLetters;
+    private int numberOfIncorrectTries;
+    private StringBuffer knownSoFar;
+
+    // constructors
+
+    public Hangman() {
+        numberOfIncorrectTries = 0;
+        secretWord = chooseSecretWord();
+        knownSoFar = new StringBuffer();
+        usedLetters = new StringBuffer();
+        MAX_ALLOWED_INCORRECT_TRIES = 6;
+
+        for (int i = 0; i < 26; i++) {
+            allLetters = allLetters.append((char) (i + 65));
+        }
+    }
+
+    // methods
+
+    /**
+     * A method to return all letters that can be used
+     *
+     * @return allLetters
+     */
+
+    public StringBuffer getAllLetters() {
+        return allLetters;
+    }
+    public StringBuffer getSecret() {
+        return secretWord;
+    }
+    public void setKnownSoFar() {
+        for (int i = 0; i < secretWord.length(); i++) {
+            knownSoFar.append('_');
+        }
+    }
+
+    /**
+     * A method to get the letters used by the player
+     *
+     * @return usedLetters as a String
+     */
+
+    public StringBuffer getUsedLetters() {
+        return usedLetters;
+    }
+
+    /**
+     * A method to get the number of incorrect tries
+     *
+     * @return numberOfIncorrectTries as an int
+     */
+
+    public int getNumOfIncorrectTries() {
+        return numberOfIncorrectTries;
+    }
+
+    /**
+     * A method to get the maximum number of incorrect tries
+     *
+     * @return MAX_ALLOWED_INCORRECT_TRIES as an int
+     */
+
+    public int getMaxAllowedIncorrectTries() {
+        return MAX_ALLOWED_INCORRECT_TRIES;
+    }
+
+    /**
+     * A method to get the letters known so far
+     *
+     * @return knownSoFar as a String
+     */
+
+    public StringBuffer getKnownSoFar() {
+        return knownSoFar;
+    }
+
+    /**
+     * A method to indicate whether the game is over or not
+     *
+     * @return the status of the game as boolean
+     */
+
+    public boolean isGameOver() {
+        return (numberOfIncorrectTries == MAX_ALLOWED_INCORRECT_TRIES || won());
+    }
+
+    /**
+     * A method to indicate whether the game is lost or not
+     *
+     * @return a boolean according to the status of the game
+     */
+
+    public boolean won() {
+        for (int i = 0; i < knownSoFar.length(); i++) {
+            if (! (knownSoFar.charAt(i) == secretWord.charAt(i)))
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * A method to choose a secret word
+     *
+     * @return secretWord as a String
+     */
+
+    public StringBuffer chooseSecretWord() {
+        final int SIZE = 10;
+        String[] words;
+        words = new String[SIZE];
+        words[0] = "artificial";
+        words[1] = "amazing";
+        words[2] = "amateur";
+        words[3] = "collapse";
+        words[4] = "computer";
+        words[5] = "science";
+        words[6] = "bilkent";
+        words[7] = "engineer";
+        words[8] = "professional";
+        words[9] = "game";
+        secretWord = new StringBuffer(words[(int) Math.floor(Math.random() * SIZE)]);
+        return secretWord;
+    }
+
+    /**
+     * A method to try a letter in the word
+     *
+     * @return the number of repetitions of the letter in the word as an int
+     */
+
+    public int tryThis(char letter) {
+        int count;
+        int count2;
+
+        count = 0;
+        count2 = 0;
+
+        for (int a = 0; a < allLetters.length(); a++) {
+            if ((allLetters.charAt(a) + "").equalsIgnoreCase(letter + ""))
+                count2++;
+        }
+        if (count2 == 0)
+            return -1;
+
+
+        for (int a = 0; a < usedLetters.length(); a++) {
+            if ((usedLetters.charAt(a) + "").equalsIgnoreCase(letter + ""))
+                return -2;
+        }
+
+        if (isGameOver())
+            return -3;
+
+        usedLetters.append(letter);
+
+        for (int a = 0; a < secretWord.length(); a++) {
+            if ((secretWord.charAt(a) + "").equalsIgnoreCase(letter + "")) {
+                knownSoFar.setCharAt(a, letter);
+
+                count++;
+            }
+        }
+        if (count == 0)
+            numberOfIncorrectTries++;
+
+        return count;
+
+    }
+
+
+}
---
+++ src/HangmanGame.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/**
+ * Main method plays the game by creating instance of Hangman class & calling its methods
+ *
+ * @author Cihan Erkan, Beyza Kalkanli, Ghada Ibrahim, Can Soygur, Burak Tos, Umair Ahmed
+ * @version 12.02.2017
+ */
+public class HangmanGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // variables
+        Hangman hangmanInstance;
+        StringBuffer secretWord;
+        char letter;
+        int letterOccurence;
+
+        // program code
+        hangmanInstance = new Hangman();
+
+        secretWord = new StringBuffer(hangmanInstance.chooseSecretWord());
+        hangmanInstance.setKnownSoFar();
+        System.out.println(hangmanInstance.getAllLetters());
+        do // The loop where the game is played.
+        {
+            System.out.println(hangmanInstance.getSecret());
+            // Prints the known-so-far and used letters.
+            System.out.println(hangmanInstance.getKnownSoFar() + "\nUsed Letters: " + hangmanInstance.getUsedLetters());
+
+            // Asks user for a guess.
+            System.out.println("Guess a letter: ");
+            letter = scan.next().charAt(0);
+
+            // Checks the given inputs and tries the letter.
+            letterOccurence = hangmanInstance.tryThis(letter);
+
+            if (letterOccurence == -1) {
+                System.out.println("Letter is not valid.");
+            } else if (letterOccurence == -2) {
+                System.out.println("Letter was already used.");
+            } else if (letterOccurence == -3) {
+                System.out.println("Game is over.");
+            }
+            System.err.println("Number:  " + hangmanInstance.getNumOfIncorrectTries());
+            System.out.println(secretWord.equals(hangmanInstance.getKnownSoFar()));
+        } while (!hangmanInstance.isGameOver()); // Checks if the game is over.
+
+        // Prints the result.
+        if (hangmanInstance.won()) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+    }
+
+}
